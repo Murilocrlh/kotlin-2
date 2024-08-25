bd9d124639be9517fd92b6f66bbfc408665d9aa2
Murilocrlh <murilodejesus.calmon.ismart@gmail.com>
2024-08-25 00:21:18 -0300

    inicioactivity.kt

--- JogoDaForca/app/src/main/java/com/example/jogodaforca/inicioactivity.kt
+++ JogoDaForca/app/src/main/java/com/example/jogodaforca/inicioactivity.kt
@@ -1,1 +1,25 @@
+package com.example.jogodaforca
 
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.jogodaforca.databinding.ActivityInicioBinding
+
+class InicioActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityInicioBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityInicioBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonJogar.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.buttonSair.setOnClickListener {
+            finishAffinity() // Fecha o aplicativo
+        }
+    }
+}
